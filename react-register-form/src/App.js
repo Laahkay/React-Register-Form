af8353d8ba9c2e6f097fbe782a9c7732fc874d4b
Laahkay <lonerkiddow15@gmail.com>
2022-10-05 09:59:45 +0200

    addiing my registration form nd displya their values

--- react-register-form/src/App.js
+++ react-register-form/src/App.js
@@ -1,124 +1,150 @@
-import React, { useState } from 'react';
-import './App.css'
-
+import React, { useRef, useState } from "react";
+import "./App.css";
 
 function Form() {
-    const [formInfo, setFormInfo] = useState({
-        name: "",
-        surname: '',
+  const [formInfo, setFormInfo] = useState({
+    FirstName: "",
+    surname:"",
+    email:"",
+    number:"",
+    Id: "",
+  });
 
-        email: ""
-    });
 
-    const [list, setList] = useState([]);
 
-    const handleChange = (e) => {
-        setFormInfo({ ...formInfo, [e.target.name]: e.target.value })
-    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setList([...list, formInfo])
-        setFormInfo({ name: '' })
-    };
+  const [list, setList] = useState([]);
+  const clear = useRef(null);
+  const handleChange = (e) => {
+    setFormInfo({ ...formInfo, [e.target.name]: e.target.value });
+  };
 
-    return (
-        <div className="form">
-            <h1>Register Form</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    name="name"
-                    value={formInfo.name}
-                /> <br />
-                <br />
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, formInfo]);
+    setFormInfo({ FirstName: "" });
+    clear.current.reset();
+  };
 
-                  <label>Surname</label>
-                    <input
-                    type="text"
-                    onChange={handleChange}
-                    name="surname"
-                    value={formInfo.surname}
-                /><br />
-                <br />
-                <label>email</label>
-                    <input
-                    type="text"
-                    onChange={handleChange}
-                    name="email"
-                    value={formInfo.email}
-                /><br />
-                <br />
-                <label>Phone Number</label>
-                    <input
-                    type="text"
-                    onChange={handleChange}
-                    name="number"
-                    value={formInfo.number}
-                /><br />
-                <br />
-                <label>Id Number</label>
-                    <input
-                    type="text"
-                    onChange={handleChange}
-                    name="id"
-                    value={formInfo.id}
-                /><br />
-                <br />
-                <button type="submit">Submit</button>
-            </form>
+  function numberOfppl(newuser) {
+    setFormInfo(formInfo.concat({ ...newuser, id: list.length + 1 }));
+  }
 
-            <table className="forms">
+  // function handleRemove(id) {
+  //   console.log(id);
+
+  //   const newList = list.filter((item) => item.id !== id);
+
+  //   setList(newList);
+  // }
+
+  return (
+    <div className="forms">
+      <form ref={clear} onSubmit={handleSubmit} className="form">
+        <h1>Register Form </h1>
+        <p>
+          Hello Please <span>Register</span>
+        </p>
+        <hr />
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="FirstName"
+          value={formInfo.FirstName}
+        />{" "}
+        <br />
+        <br />
+        <label>Surname</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="surname"
+          value={formInfo.surname}
+        />
+        <br />
+        <br />
+        <label>email</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="email"
+          value={formInfo.email}
+        />
+        <br />
+        <br />
+        <label>Phone Number</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="number"
+          value={formInfo.number}
+        />
+        <br />
+        <br />
+        <label>Id Number</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="Id"
+          value={formInfo.Id}
+        />
+        <br />
+        <br />
+        <button type="submit" onSubmit={numberOfppl} className="butt">
+          Submit
+        </button>
+      </form>
+      <br />
+      <h2 className="center">Display Result</h2>
+      <div className="table">
+        <h2 className="text-xl">Num Of loggers ({list.length})</h2>
+        <hr></hr>
+        <table>
           <thead>
-            <tr className="border-b">
-              <th className="">Name</th>
-              <br />
-              <th className="">Surname</th>
-              <br /> 
-              <th className="">Number</th>
-              <br /> 
-              <th className="">Email</th>
-              <br /> 
-              <th className="">ID Number</th>
+            <tr>
+              <th className="one">Name</th>
+
+              <th className="two">Surname</th>
+
+              <th className="three">Phone</th>
+
+              <th className="four">Email</th>
+              <th className="four">ID Number</th>
             </tr>
           </thead>
           <tbody>
-          {list.map((item) => {
-                    return (
-                        <tr>
-                            <td>{item.name}</td>
-                            <br />
-                            <td> {item.surname}</td>
-                            <br />
-                            <td> {item.number}</td>
-                            <br />
-                            <td> {item.email}</td>
-                            <br />
-                            <td> {item.id}</td>
-                        </tr>)
-                })}
+            {list.map((item) => {
+              return (
+                <tr className="" key={item.id}>
+                  
+                  <td>{item.FirstName}</td>
+
+                  <td> {item.surname}</td>
+
+                  <td> {item.number}</td>
+
+                  <td> {item.email}</td>
+
+                  <td> {item.Id}</td>
+                  {/* <button
+                    type="button"
+                    className="butts"
+                    onClick={() => handleRemove(item.id)}
+                  >
+                    Delete
+                  </button> */}
+                </tr>
+                
+              );
+          
+            })}
           </tbody>
         </table>
-{/* 
-            <ul>
-                {list.map((item) => {
-                    return (
-                        <li>
-                            <h2>Name :{item.name}</h2>
-                            <h2>Surname: {item.surname}</h2>
-                            <h2>Number: {item.number}</h2>
-                            <h2>Email : {item.email}</h2>
-                            <h2> ID Number: {item.id}</h2>
-                        </li>)
-                })}
-            </ul> */}
+      </div>
+    </div>
+  );
+}
 
-        </div>
-
-    )
-};
-
-export default Form
+export default Form;
